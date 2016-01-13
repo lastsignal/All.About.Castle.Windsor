a0d37320b8abf641e656b16bb6f9ddf0f2c102b9
Dan Da'neshgar <dan@lastsignal.com>
2016-01-13 14:35:22 -0600

    BaseService dependency

---
+++ ClassLibrary/BaseClass/BaseClassRegistrationtest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using NUnit.Framework;
+
+namespace ClassLibrary.BaseClass
+{
+	[TestFixture]
+	public class BaseClassRegistrationtest : TestBase
+	{
+		[TestFixtureSetUp]
+		public void Startup()
+		{
+			Container.Register(
+				Component.For<IAService>().ImplementedBy<AService>(),
+				Component.For<IBService>().ImplementedBy<BService>(),
+				Component.For<IApplicationService>().ImplementedBy<ApplicationServcie>());
+		}
+
+		[Test]
+		public void Base_dependant_service_should_defined()
+		{
+			var service = Container.Resolve<IApplicationService>();
+
+			service.IsADefined().ShouldBeTrue();
+		}
+
+		[Test]
+		public void Local_dependant_service_should_defined()
+		{
+			var service = Container.Resolve<IApplicationService>();
+
+			service.IsBDefined().ShouldBeTrue();
+		}
+
+		[Test]
+		public void Report_Identifications()
+		{
+			var service = Container.Resolve<IApplicationService>();
+
+			Console.WriteLine(service.WhoAmI());
+			Console.WriteLine(service.WhoIsA());
+			Console.WriteLine(service.WhoIsB());
+		}
+	}
+}
---
+++ ClassLibrary/BaseClass/ServiceBase.cs
@@ -0,0 +1,73 @@
+﻿namespace ClassLibrary.BaseClass
+{
+	public interface IAService
+	{
+		string WhoAmI();
+	}
+
+	public interface IBService
+	{
+		string WhoAmI();
+	}
+
+	public interface IApplicationService
+	{
+		string WhoAmI();
+		bool IsADefined();
+		bool IsBDefined();
+
+		string WhoIsA();
+		string WhoIsB();
+	}
+
+	public class AService : IAService
+	{
+		public string WhoAmI()
+		{
+			return "I am Service A";
+		}
+	}
+
+	public class BService : IBService
+	{
+		public string WhoAmI()
+		{
+			return "I am Service B";
+		}
+	}
+
+	public class ServiceBase
+	{
+		public IAService AService { get; set; }
+	}
+
+	public class ApplicationServcie : ServiceBase, IApplicationService
+	{
+		public IBService BService { get; set; }
+
+		public string WhoAmI()
+		{
+			return "I am Application Service";
+		}
+
+		public bool IsADefined()
+		{
+			return AService != null;
+		}
+
+		public bool IsBDefined()
+		{
+			return BService != null;
+		}
+
+		public string WhoIsA()
+		{
+			return AService.WhoAmI();
+		}
+
+		public string WhoIsB()
+		{
+			return BService.WhoAmI();
+		}
+	}
+}
